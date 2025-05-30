b286b4993c7b843d753032c8f7fa3739ab1eb9e1
Piyush Channawar <channawarpiyush@gmail.com>
2025-05-31 01:45:03 +0530

    feat: add HeroSection component with course search and exploration features

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import { Button } from "@/components/ui/button"
 import Login from "./pages/Login"
 import Navbar from "./components/Navbar"
+import HeroSection from "./pages/student/HeroSection"
 
 function App() {
   return (
     <main>
       <Navbar />
+      <HeroSection />
       <Login />
     </main>
   )
